8f15685dd8b503677f8caa0c0d3d4386f04aadbc
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-10-09 01:14:01 -0400

    created routes needed for user login, added handlebars, adjusted server.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     res.render('homepage', {
         // id: 1,
         // post_url: 'https://handlebars.com/guide',
@@ -12,6 +15,14 @@ router.get('/', (req, res) => {
         //     username: 'test_user'
         // }
     });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const path = require('path');
 const express = require('express');
-const sequelize = require('./config/connection');
 // const routes = require('./controllers/');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-// const session = require('express-session');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -21,17 +35,4 @@ sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
-
-// app.use(session(sess));
 // app.use(routes);
