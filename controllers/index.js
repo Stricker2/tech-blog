d7a4ec8415df32807881621abc372d403e9e1bab
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-10-08 22:27:35 -0400

    add homepage, add api index, update server.js to function with current code

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+
 const homeRoutes = require('./home-routes.js');
+const apiRoutes = require('./api/');
 
 router.use('/', homeRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
-const sequelize = require('sequelize');
-const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+// const routes = require('./controllers/');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-const session = require('express-session');
+// const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,21 +15,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
+// const sess = {
+//     secret: 'Super secret secret',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
 
-app.use(session(sess));
-app.use(routes);
+// app.use(session(sess));
+// app.use(routes);
